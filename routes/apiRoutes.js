9ccc05981c2cc5396465cd77809b76faf2eaef59
anil kumar <anil.kumar093@outlook.com>
2018-08-02 01:24:33 -0400

    database is working

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,24 +1,80 @@
 var db = require("../models");
 
 module.exports = function(app) {
-  // Get all examples
-  app.get("/api/examples", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.json(dbExamples);
-    });
+  // Get all posts
+  app.get("/api/posts", function(req, res) {
+    db.Post.findAll().then(function(dbpost) {
+      res.json(dbpost);
+    }).catch(function(error){
+      res.json('{"Error":could not find stuff}')
+    })
   });
 
-  // Create a new example
-  app.post("/api/examples", function(req, res) {
-    db.Example.create(req.body).then(function(dbExample) {
-      res.json(dbExample);
-    });
+  // Get post by name
+  app.get("/api/posts/:name", function(req, res) {
+    db.Post.findAll({
+      where: {
+        name: req.params.name
+      }
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
+  
+  
+  //Get route for retrieving a single post
+
+
+  app.get("/api/posts/:name", function(req, res) {
+    db.Post.findOne({
+      where: {
+        name: req.params.name
+      }
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
   });
 
-  // Delete an example by id
-  app.delete("/api/examples/:id", function(req, res) {
-    db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.json(dbExample);
-    });
+
+  // create new post 
+  app.post("/api/posts", function(req, res) {
+    console.log(req.body);
+    db.Post.create({
+      name: req.body.name,
+      userName: req.body.userName,
+      email: req.body.email,
+      password: req.body.password,
+      password2:req.body.password2
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
   });
+
+ 
+
+  // Delete an post by an name
+
+
+  app.delete("/api/posts/:name", function(req, res) {
+    db.Post.destroy({
+      where: {
+        name: req.params.name
+      }
+    })
+      .then(function(dbPost) {
+        res.json(dbPost);
+      });
+  });
+
+  // app.delete("/api/examples/:id", function(req, res) {
+  //   db.Example.destroy({ where: { id: req.params.id } }).then(function(dbExample) {
+  //     res.json(dbExample)
+  //   });
+  // });
 };
+
+
+
